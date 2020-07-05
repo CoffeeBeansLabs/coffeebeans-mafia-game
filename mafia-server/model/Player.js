2393d30416353bf30e53c0d7a603fd8a94dc9f08
indraneel rao <indraneel@coffeebeans.io>
2020-07-05 19:15:33 +0530

    adds role

--- mafia-server/model/Player.js
+++ mafia-server/model/Player.js
@@ -3,11 +3,11 @@ const {nanoid} = require('nanoid')
 module.exports = class Player {
   isCaptain
 
-  constructor (name,isCaptain,token) {
+  constructor (name,isCaptain,token,role) {
     this.isCaptain = isCaptain
     this.name = name
     this.id = nanoid()
-    this.role = ""
+    this.role = role
     this.token = token
   }
 }
